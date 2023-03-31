7cdea583c3135ea52196ad115d44f880a760b9e5
chemitaro <iwasi_44@hotmail.com>
2023-03-31 09:31:58 +0900

    AIの出力するテキストに色付け、アクセス中の一時的な情報

--- lib/main.py
+++ lib/main.py
@@ -1,7 +1,7 @@
 # チャットを行う実行プログラム
-
-from chat_bot import SimpleChatBot
+import sys
 from termcolor import colored
+from chat_bot import SimpleChatBot
 from text_to_speech import text_to_siri_async, text_to_voicevox_async
 
 # チャットの実行
@@ -11,11 +11,20 @@ if __name__ == '__main__':
     while True:
         print("")
         user_input = input("あなた: ")
+        print("")
 
         if user_input.lower() in ["終了", "exit"]:
             break
 
+        sys.stdout.write(colored("Accessing...", "blue"))
+        sys.stdout.flush()
+
         ai_message = simple_chat_bot.talk(user_input).content
-        print("")
-        print(colored(f"AI: {ai_message}", "magenta"))
-        text_to_voicevox_async(ai_message, speaker=19)
+
+        sys.stdout.write("\r")  # テキストの先頭に戻る
+        sys.stdout.write("            ")  # テキストを消去
+        sys.stdout.write("\r")  # テキストの先頭に戻る
+        sys.stdout.flush()
+
+        print(colored(f"AI: {ai_message}", "blue"))
+        text_to_voicevox_async(ai_message, speaker=47)
